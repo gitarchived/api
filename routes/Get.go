61273784a04c7eebe1c394788781f7540a4f5964
vclemenzi <vclemenzi@users.noreply.github.com>
2024-02-04 09:18:48 +0100

    feat: add `get` endpoint

---
+++ routes/Get.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"time"
+
+	"github.com/gitarchived/api/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func Get(c *fiber.Ctx, db *gorm.DB) error {
+	host := c.Params("host")
+	owner := c.Params("owner")
+	name := c.Params("name")
+	fullName := owner + "/" + name
+
+	if owner == "" || name == "" {
+		return c.Status(400).JSON(fiber.Map{
+			"status":  400,
+			"message": "Bad Request",
+		})
+	}
+
+	var results models.Repository
+
+	data := db.Where("host = ? AND name = ?", host, fullName).First(&results)
+
+	if data.Error != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"status":  500,
+			"message": "Internal Server Error",
+		})
+	}
+
+	formattedResults := models.RepositoryResponse{
+		ID:        results.ID,
+		Name:      results.Name,
+		Host:      results.Host,
+		CreatedAt: results.CreatedAt.Format(time.RFC3339),
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status":  200,
+		"message": "OK",
+		"data":    formattedResults,
+	})
+}
